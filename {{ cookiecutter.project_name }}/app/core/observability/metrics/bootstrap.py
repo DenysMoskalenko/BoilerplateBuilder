48297c29c0a2aa05687a6b4273884001a7fa12c2
Laskarzhevskiy Eduard <laskarzhevskyi.eduard@gmail.com>
2026-05-30 22:37:31 +0200

    refactor: remove redundant latency metric

--- {{ cookiecutter.project_name }}/app/core/observability/metrics/bootstrap.py
+++ {{ cookiecutter.project_name }}/app/core/observability/metrics/bootstrap.py
@@ -4,7 +4,7 @@ import logging
 from fastapi import FastAPI
 from prometheus_client import make_asgi_app
 from prometheus_fastapi_instrumentator import PrometheusFastApiInstrumentator
-from prometheus_fastapi_instrumentator.metrics import default, latency
+from prometheus_fastapi_instrumentator.metrics import default
 
 _logger = logging.getLogger(__name__)
 
@@ -12,7 +12,6 @@ _logger = logging.getLogger(__name__)
 def setup(app: FastAPI) -> None:
     instrumentator = PrometheusFastApiInstrumentator(should_group_status_codes=True, should_ignore_untemplated=True)
     instrumentator.add(default())
-    instrumentator.add(latency())
     instrumentator.instrument(app)
 
     app.mount('/metrics/', make_asgi_app())
