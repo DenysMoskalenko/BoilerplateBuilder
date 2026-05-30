17fcee8f8505c6623ac3d8a2a5b6800f9569096d
Laskarzhevskiy Eduard <laskarzhevskyi.eduard@gmail.com>
2026-05-30 21:33:45 +0200

    refactor: simplify structured log timestamp

--- {{ cookiecutter.project_name }}/app/core/logging/models.py
+++ {{ cookiecutter.project_name }}/app/core/logging/models.py
@@ -1,18 +1,8 @@
 from enum import auto, StrEnum
-from typing import Annotated
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel
 
-LOG_TIMESTAMP_PATTERN = r'^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}[+-]\d{4}$'
 LOG_TIMESTAMP_FORMAT = '%Y-%m-%dT%H:%M:%S%z'
-TimestampWithOffset = Annotated[
-    str,
-    Field(
-        pattern=LOG_TIMESTAMP_PATTERN,
-        examples=['2026-05-29T10:00:00+0000', '2026-05-29T12:30:00+0230'],
-        description='ISO timestamp with numeric UTC offset',
-    ),
-]
 
 
 class LogFormatType(StrEnum):
@@ -21,7 +11,7 @@ class LogFormatType(StrEnum):
 
 
 class StructuredLogRecord(BaseModel):
-    timestamp: TimestampWithOffset
+    timestamp: str
     logger_name: str
     level: str
     message: str
